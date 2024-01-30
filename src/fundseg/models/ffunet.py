4fbc9fe6d082e7e0f629c0ff2c9244ea3cd9b084
Clément Playout <clpla@slurm.info.polymtl.ca>
2024-01-30 13:55:29 -0500

    Fix gitignore

--- src/fundseg/models/ffunet.py
+++
@@ -1,6 +0,0 @@
-"""
-Implementation of the FFUNET model. FFU-Net: Feature Fusion U-Net for Lesion Segmentation of Diabetic Retinopathy by Y. Xu and al.
-
-"""
-
-# TODO
--- src/fundseg/scripts/train_script.sh
+++
@@ -1,4 +0,0 @@
-
-
-python train.py --data_aug_ops heavy --optimizer sgd --log_dice False --dice_smooth 0.4 --lr 0.003 --model unet_timm-efficientnet-b4 --dataset all
-python train.py --data_aug_ops heavy --optimizer sgd --log_dice False --dice_smooth 0.4 --lr 0.003 --model fpn_timm-efficientnet-b4 --dataset all
