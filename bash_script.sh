903b31c611057a8bc6c24179fc7b05119ca5e478
ClementPla <clement.playout@polymtl.ca>
2024-06-14 10:21:00 -0400

    Commit latest changes

--- bash_script.sh
+++ bash_script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# python src/adptSeg/scripts/linear_probing.py --position 1 --feature_type encoder
-# python src/adptSeg/scripts/linear_probing.py --position 1 --feature_type decoder
-# python src/adptSeg/scripts/linear_probing.py --position 2 --feature_type encoder
-# python src/adptSeg/scripts/linear_probing.py --position 2 --feature_type decoder
-# python src/adptSeg/scripts/linear_probing.py --position 3 --feature_type encoder
+
+python src/adptSeg/scripts/linear_probing.py --position 4 --feature_type decoder
+
+python src/adptSeg/scripts/linear_probing.py --position 0 --feature_type encoder
+python src/adptSeg/scripts/linear_probing.py --position 0 --feature_type decoder
+
+python src/adptSeg/scripts/linear_probing.py --position 1 --feature_type encoder
+python src/adptSeg/scripts/linear_probing.py --position 1 --feature_type decoder
+python src/adptSeg/scripts/linear_probing.py --position 2 --feature_type encoder
+python src/adptSeg/scripts/linear_probing.py --position 2 --feature_type decoder
+python src/adptSeg/scripts/linear_probing.py --position 3 --feature_type encoder
+python src/adptSeg/scripts/linear_probing.py --position 3 --feature_type decoder
+python src/adptSeg/scripts/linear_probing.py --position 4 --feature_type encoder
+
 python src/adptSeg/scripts/linear_probing.py --position 5 --feature_type encoder
-# python src/adptSeg/scripts/linear_probing.py --position 4 --feature_type encoder
-# python src/adptSeg/scripts/linear_probing.py --position 4 --feature_type decoder
-# python src/adptSeg/scripts/linear_probing.py --position 0 --feature_type encoder
-# python src/adptSeg/scripts/linear_probing.py --position 0 --feature_type decoder
